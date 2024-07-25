04be59d65392c02d2d6f93ea080e0f4dd8520e93
Gareth Ng <njwqz77@gmail.com>
2024-07-25 22:58:53 +0800

    :recycle: Add src/

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useReducer } from "react"
 import { Card, Button } from '@nextui-org/react';
-
+import "./style.css"
 const Popup = () => {
+  const [count, increase] = useReducer((c) => c + 1, 0)
   return (
     <Card>
-      <h1>AI Input Translator</h1>
-      <Button variant="primary">Click Me</Button>
+     
     </Card>
   );
 };
